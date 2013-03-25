06943de57cadc36503c9acf6a04d00873a80082f
Zak Johnson <zakj@nox.cx>
2013-03-24 17:02:38 -0700

    Add logging configuration

--- collate/__init__.py
+++ collate/__init__.py
@@ -1,12 +1,37 @@
+import logging
+
 from flask import Flask
 from flask.ext.assets import Bundle, Environment
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
+    app.config.update(
+        ADMINS=['zak@madebycabin.com'],
+        SMTP_HOST='email-smtp.us-east-1.amazonaws.com',
+        SMTP_FROM='Collate <xo@madebycabin.com>',
+    )
     app.config.from_pyfile('settings.cfg')
+    configure_logging(app)
     register_assets(app)
     return app
+
+
+def configure_logging(app):
+    if not app.debug:
+        admins = app.config['ADMINS']
+        if isinstance(admins, basestring):
+            admins = [admins]
+        mail_handler = logging.handlers.SMTPHandler(
+            mailhost=app.config['SMTP_HOST'],
+            fromaddr=app.config['SMTP_FROM'], toaddrs=admins,
+            subject='Error',
+            credentials=app.config.get('SMTP_CREDENTIALS'), secure=())
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+    # StreamHandler is suitable for the dev server and uWSGI.
+    handler = logging.StreamHandler()
+    app.logger.addHandler(handler)
 
 
 def register_assets(app):
